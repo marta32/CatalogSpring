64c6a9deb719791f1ea88be6256ece69bc554732
Marta Enachioiu <marta.enachioiu@gmail.com>
2022-10-29 17:41:48 +0300

    Create Subject Entity

---
+++ src/main/java/com/example/springbootcatalog/entity/Subject.java
@@ -0,0 +1,24 @@
+package com.example.springbootcatalog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "subjects")
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Integer idTeacher;
+}
