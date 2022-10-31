bd921c3e8267e0104944920bea04dce3ce5676eb
Marta Enachioiu <marta.enachioiu@gmail.com>
2022-10-31 12:14:07 +0200

    Create TeacherResponse class

---
+++ src/main/java/payload/TeacherResponse.java
@@ -0,0 +1,19 @@
+package payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeacherResponse {
+    private List<TeacherDto> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
